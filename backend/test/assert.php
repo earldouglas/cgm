96f1646c510591b860228cef825ac7d55efb06eb
James Earl Douglas <james@earldouglas.com>
2025-08-16 09:39:43 -0700

    Scala -> PHP for great serverlessness

---
+++ backend/test/assert.php
@@ -0,0 +1,14 @@
+<?php
+
+  function assertEquals($name, $expected, $observed) {
+    if ($expected === $observed) {
+      echo "✔ $name\n";
+    } else {
+      echo "✖ $name\n";
+      echo "  expected: $expected\n";
+      echo "  observed: $observed\n";
+      exit(1);
+    }
+  }
+
+?>
