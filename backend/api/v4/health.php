3cad3aa1e306c18377b284304481774d3553be5c
James Earl Douglas <james@earldouglas.com>
2025-08-23 13:23:50 -0700

    /api/v4/health: move JSON encoding from lib to api

--- backend/api/v4/health.php
+++ backend/api/v4/health.php
@@ -1,7 +1,24 @@
 <?php
 
-  require_once dirname(__FILE__) . '/../../lib/health.php';
+  function getHealthJson() {
 
-  echo getHealth();
+    require_once dirname(__FILE__) . '/../../lib/health.php';
+
+    $health = getHealth();
+
+    $json =
+      json_encode(
+        $health,
+        JSON_PRETTY_PRINT
+      );
+
+    return $json;
+  }
+
+  if (isset($_SERVER['REQUEST_METHOD'])) {
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+      echo getHealthJson();
+    }
+  }
 
 ?>
