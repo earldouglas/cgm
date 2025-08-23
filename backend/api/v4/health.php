35f13c4cf12a4eccd90b38bf6ed885c414843f15
James Earl Douglas <james@earldouglas.com>
2025-08-23 13:25:28 -0700

    /api/v4/health: use curl to test

--- backend/api/v4/health.php
+++ backend/api/v4/health.php
@@ -21,4 +21,19 @@
     }
   }
 
+  if (getenv('TEST') !== false) {
+
+    require_once dirname(__FILE__) . '/../../test/assert.php';
+    require_once dirname(__FILE__) . '/../../lib/curl.php';
+
+    $response = get('http://localhost:8888/api/v4/health');
+    $health = json_decode($response, true);
+
+    $name = 'Health check';
+    $expected = 'ok';
+    $observed = $health['db'];
+
+    assertEquals($name, $expected, $observed);
+  }
+
 ?>
---
+++ backend/lib/curl.php
@@ -0,0 +1,28 @@
+<?php
+
+  function get($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    return $response;
+  }
+
+  if (getenv('TEST') !== false) {
+
+    require_once dirname(__FILE__) . '/../test/assert.php';
+
+    $response = get('https://earldouglas.com');
+
+    preg_match('/<title>([^<]*)<\/title>/', $response, $matches);
+
+    $name = 'GET https://earldouglas.com';
+    $expected = 'James Earl Douglas';
+    $observed = $matches[1];
+
+    assertEquals($name, $expected, $observed);
+  }
+?>
